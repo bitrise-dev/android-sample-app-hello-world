71183127745fd01ebac9f418327dff055ca35338
Rob Hedgpeth <rob.hedgpeth@gmail.com>
2022-05-06 16:17:54 -0500

    Initial additions

---
+++ app/src/main/java/com/rhedgpeth/hello_world/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.rhedgpeth.hello_world
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+        setContentView(R.layout.activity_main)
+    }
+}
